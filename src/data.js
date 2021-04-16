3bcb4dba742bfe24c7f815676695fb0358c0839a
jesukoseemani <olowosusiayo@gmail.com>
2021-04-16 23:58:13 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,6 +2,66 @@ import { v4 as uuidv4 } from "uuid";
 function chillHop() {
   return [
     {
+      name: "World",
+      cover:
+        "https://www.naijaloaded.com.ng/wp-content/uploads/2021/04/Bella-Shmurda-World.jpg",
+      artist: "Bella Shmurda",
+      audio: "https://naijaloaded.store/wp-content/uploads/2021/04/Bella-Shmurda-World.mp3?_=1",
+      color: ["#2D4A76", "#6A9336"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Matrimony",
+      cover:
+        "https://www.naijaloaded.com.ng/wp-content/uploads/2021/04/rum-and-boogie.jpeg",
+      artist: "Peruzzi Ft. Tiwa Savage",
+      audio: "https://naijaloaded.store/wp-content/uploads/2021/04/Peruzzi-Ft.-Tiwa-Savage-Matrimony.mp3?_=1",
+      color: ["#010101", "#6C3322"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Keep Going",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://music.audiomack.com/albums/adekunlegold/afro-pop-vol-1/streaming/okay.mp3?Expires=1617563957&Signature=GjTtZe42ldtcmZ4iC2b0kpWF2MjlRcqqlkPPy-LPl01xqvGI0nHsLIRuiGg2wNdUufTwyA7g9ogQEvCITv2oAf1wdM2VzhPgXhTAAgtWUMhM6pk3AzGXDrRWiA3iovF6677XTeLP5nzglSdNbTJ-tnRr6f8nnQiKXBWAC3zAbEE_&Key-Pair-Id=APKAIKAIRXBA2H7FXITA",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Nightfall",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://naijaloaded.store/wp-content/uploads/2021/03/Danny-S-Jah.mp3?_=1",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Reflection",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Under the City Stars",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+     {
       name: "Beaver Creek",
       cover:
         "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
@@ -61,7 +121,66 @@ function chillHop() {
       id: uuidv4(),
       active: false,
     },
-    //ADD MORE HERE
+     {
+      name: "Beaver Creek",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Daylight",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Keep Going",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://music.audiomack.com/albums/adekunlegold/afro-pop-vol-1/streaming/okay.mp3?Expires=1617563957&Signature=GjTtZe42ldtcmZ4iC2b0kpWF2MjlRcqqlkPPy-LPl01xqvGI0nHsLIRuiGg2wNdUufTwyA7g9ogQEvCITv2oAf1wdM2VzhPgXhTAAgtWUMhM6pk3AzGXDrRWiA3iovF6677XTeLP5nzglSdNbTJ-tnRr6f8nnQiKXBWAC3zAbEE_&Key-Pair-Id=APKAIKAIRXBA2H7FXITA",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Nightfall",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://naijaloaded.store/wp-content/uploads/2021/03/Danny-S-Jah.mp3?_=1",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Reflection",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Under the City Stars",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
   ];
 }
 
